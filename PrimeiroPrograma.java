a09c9d91a8d9113cb652302135bbaeed7ac62754
Alexandre dos Santos Sanches <sanches.ale.3018@gmail.com>
2022-11-25 23:34:50 -0300

    First commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	System.out.println("Hello Word");
+	}*/
+	
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+
+}
+class Livro{
+	private String nome;
+	private String npag;
+	}
